c885018328e3a03ea83808e0afb0b02b96dfc70c
02dejulho <lucianamts.blog@gmail.com>
2024-09-18 14:43:20 -0300

    Alterando algumas configurações do TypeScript

--- TypeScript, POO e Browser APIs/aula17/dist/script.js
+++ TypeScript, POO e Browser APIs/aula17/dist/script.js
@@ -3,3 +3,8 @@ const numero1 = 10;
 const numero2 = 20;
 console.log("TypeScript");
 console.log(numero1 + numero2);
+//----------------------------------
+function somar(num1, num2) {
+    return num1 + num2;
+}
+somar(15, 20);
--- TypeScript, POO e Browser APIs/aula17/script.ts
+++ TypeScript, POO e Browser APIs/aula17/script.ts
@@ -3,3 +3,11 @@ const numero2 = 20
 
 console.log("TypeScript")
 console.log(numero1 + numero2)
+
+//----------------------------------
+
+function somar(num1:number, num2:number) {
+  return num1 + num2
+}
+
+somar (15, 20)
